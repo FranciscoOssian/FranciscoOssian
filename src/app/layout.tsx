ae20c4d0b8d5a256fbf01062bde5458b8cd8e8d7
Francisco Ossian <ossian.neto@gmail.com>
2023-07-17 16:30:12 -0300

    refact

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Francisco Ossian',
+  description: '',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blog">blog</a></li>
+            <li><a href="/projects">projects</a></li>
+            <li><a href="/contact">contact</a></li>
+          </ul>
+        </nav>
+        {children}
+      </body>
+    </html>
+  )
+}
